ada64bb333e69b2aec062244641f67400e682e7c
fxq3144807033 <54670447+fxq3144807033@users.noreply.github.com>
2023-04-02 20:48:32 +0000

    update ui

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'http://blog.fengxiangqiu.com/wp-content/uploads/2023/04/2d18ffaacae599b99a91243b09c902ce.jpg',
+      name: 'ChatGpt',
+      description: '',
     },
   }
 }
